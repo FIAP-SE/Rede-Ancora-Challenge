7378f3c0f3104b73a0f96d136752eef4d47a629c
andré <andr3.luiz.viana@gmail.com>
2024-05-05 02:03:42 -0300

    feat: adição de animação nas rotas das páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import { animated, useTransition } from 'react-spring';
 import Home from "./routes/Home";
 import SearchByPlate from "./routes/Home/SearchByPlate";
 import SearchByFilter from "./routes/Home/SearchByFilter";
@@ -12,7 +13,24 @@ import ClientPage from "./routes/Home/ClientPage";
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
+      <AnimatedRoutes />
+    </BrowserRouter>
+  );
+}
+
+function AnimatedRoutes() {
+  const location = useLocation();
+  const transitions = useTransition(location, {
+    initial: { opacity: 1, transform: 'translateX(0%)' },
+    from: { opacity: 0, transform: 'translateX(100%)' },
+    enter: { opacity: 1, transform: 'translateX(0%)' },
+    leave: { opacity: 0, transform: 'translateX(-100%)' },
+    keys: location.pathname
+  });
+
+  return transitions((style, item) => (
+    <animated.div style={{ ...style, position: 'absolute', width: '100%' }}>
+      <Routes location={item}>
         <Route path="/" element={<Home />} />
         <Route path="/searchbyplate" element={<SearchByPlate />} />
         <Route path="/searchbyfilter" element={<SearchByFilter />} />
@@ -23,6 +41,6 @@ export default function App() {
         <Route path="/registration" element={<Registration />} />
         <Route path="/clientpage" element={<ClientPage />} />
       </Routes>
-    </BrowserRouter>
-  );
+    </animated.div>
+  ));
 }
