ecf08d6327aa1d8b4502d29bf32570f47e463669
Paulo Sergio <paulosergioilha38@gmail.com>
2024-05-10 20:53:30 -0300

    Botão voltar productDetail

--- src/components/CardProductDetail/index.tsx
+++ src/components/CardProductDetail/index.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import ButtonBuy from '../ButtonBuy';
 import QuantityModifier from '../ButtonQuantityModifier';
+import voltarImg from "../../../public/assets/voltar.svg"
 import './styles.css';
+import { Link } from 'react-router-dom';
 
 type Props = {
     imagePath: string;
@@ -26,20 +28,28 @@ export default function CardProductDetail({ imagePath, name, price, descricao, m
     };
 
     return (
-        <div className='rda-card-container'>
-            <div className='rda-card-left'>
-                <img src={imagePath} alt="Product" />
-                <h2>{name}</h2>
-                <h6><span className='rda-moeda-detail'>R$</span> {price}</h6>
+        <div>
+            <div className='rda-voltar'>
+                <Link to={"/searchcatalog"} className='rda-icon-voltar'>
+                    <img src={voltarImg} alt="icon" />
+                    <h3> VOLTAR </h3>
+                </Link>
             </div>
-            <div className='rda-card-right'>
-                <p>Descrição:</p>
-                <p>{descricao}</p>
-                <p><span>Marca: </span>{marca}</p>
-                <p><span>Outras informações: </span>{outrasInfos}</p>
-                <div className='rda-qtd-buy'>
-                    <QuantityModifier quantity={quantity} onIncrement={handleIncrement} onDecrement={handleDecrement} />
-                    <ButtonBuy />
+            <div className='rda-card-container'>
+                <div className='rda-card-left'>
+                    <img src={imagePath} alt="Product" />
+                    <h2>{name}</h2>
+                    <h6><span className='rda-moeda-detail'>R$</span> {price}</h6>
+                </div>
+                <div className='rda-card-right'>
+                    <p>Descrição:</p>
+                    <p>{descricao}</p>
+                    <p><span>Marca: </span>{marca}</p>
+                    <p><span>Outras informações: </span>{outrasInfos}</p>
+                    <div className='rda-qtd-buy'>
+                        <QuantityModifier quantity={quantity} onIncrement={handleIncrement} onDecrement={handleDecrement} />
+                        <ButtonBuy />
+                    </div>
                 </div>
             </div>
         </div>
